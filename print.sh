8eeac1bcae8c810b467f9f6f855ad586bb04dea5
vasanthi-dev <vasanthi2dev@gmail.com>
2022-02-22 10:05:39 -0500

    print the multiple lines with color

--- print.sh
+++ print.sh
@@ -2,3 +2,13 @@
 
 echo hello world
 echo Hi this is shell scripting
+
+
+#print lines in colors
+
+echo -e "\e[31mHello World In Red Color\e[0m"
+echo -e "\e[32mHello World In Red Color\e[0m"
+echo -e "\e[33mHello World In Red Color\e[0m"
+echo -e "\e[34mHello World In Red Color\e[0m"
+echo -e "\e[35mHello World In Red Color\e[0m"
+echo -e "\e[36mHello World In Red Color\e[0m"
