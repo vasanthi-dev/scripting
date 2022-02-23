5a9d72006b061de46865a3b598e1037456e997c2
vasanthi-dev <vasanthi2dev@gmail.com>
2022-02-23 10:03:22 -0500

    Adding Roboshop Project

---
+++ roboshop/frontend.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Instaling Nginx"
+
+yum install nginx -y
+
+echo "Enable Nginx"
+
+systemctl enable nginx
+
+echo "Start Nginx"
+
+systemctl start nginx
+
+echo Download the Content
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+echo Deploy in Nginx Default Location
+
+cd /usr/share/nginx/html
+
+echo "Remove All Files"
+
+rm -rf *
+
+echo "Unzip the Folder"
+
+unzip /tmp/frontend.zip
+
+echo "Move the folder to current location"
+mv frontend-main/* .
+mv static/* .
+
+echo "Remove the files which are not used"
+rm -rf frontend-master static README.md
+
+echo "Move local Configuration to webserver location"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
