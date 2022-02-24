e9fb2fec2db32241e43972e02985963760371bcc
vasanthi-dev <vasanthi2dev@gmail.com>
2022-02-24 09:42:56 -0500

    frontend setup

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,31 +5,34 @@ source components/commons.sh
 echo "Frontend Setup"
 yum install nginx -y &>>LOG_FILE
 STAT_CHECK $? "Nginx Installation"
-#
-#echo "Enable Nginx"
-#
-#systemctl enable nginx
-#
-#echo "Start Nginx"
-#
-#systemctl start nginx
-#
-#echo Download the Content
-#
-#curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-#
-#echo Deploy in Nginx Default Location
-#
-#cd /usr/share/nginx/html
-#
-#echo "Remove All Files"
-#
-#rm -rf *
-#
-#echo "Unzip the Folder"
-#
-#unzip /tmp/frontend.zip
-#
+echo "Download the Content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
+STAT_CHECK $? "Download frontend Content"
+echo "Remove All Files"
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE
+STAT_CHECK $? "Remove All html content Files"
+
+echo "Unzip the Folder"
+unzip -o -d /tmp /tmp/frontend.zip &>>LOG_FILE
+STAT_CHECK $? "unzip the folder to tmp loacation"
+
+echo "Move files to nginx path"
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/.
+STAT_CHECK $? "Move files to nginx path"
+
+echo "Copy Roboshop Configuration File"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Copy Roboshop Configuration File"
+
+
+echo "Enable Nginx"
+systemctl enable nginx
+STAT_CHECK $? "Enable Nginx"
+
+echo "Start Nginx"
+systemctl start nginx
+STAT_CHECK $? "Start Nginx"
+
 #echo "Move the folder to current location"
 #mv frontend-main/* .
 #mv static/* .
