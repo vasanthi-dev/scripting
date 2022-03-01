7f3c586296348b8d501abd26a57015bc54aed3a6
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-01 04:20:16 -0500

    catalogue component

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -8,7 +8,24 @@ echo "Catalogue Setup"
 yum install nodejs make gcc-c++ -y &>>${LOG_FILE}
 STAT_CHECK $? "Install nodejs"
 
+id roboshop &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
 useradd roboshop &>>${LOG_FILE}
 STAT_CHECK $? "Add Application User"
+fi
 
 Download catalogue
+
+mv /tmp/catalogue-main/* /home/roboshop/catalogue
+STAT_CHECK $? "Move Catalogue Content"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>${LOG_FILE}
+STAT_CHECK $? "Install NodeJs Dependencies"
+
+chown roboshop:roboshop -R /home/roboshop
+
+sed -i -e 's/MONGO_DNSNAME/mongo.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
+STAT_CHECK $? "Updated DNSNAME in SystemD"
+
+echo "\e[1mService Enable And Start\e[0m"
+systemctl daemon-reload &>>${LOG_FILE} && systemctl enable catalogue &>>${LOG_FILE} && systemctl start catalogue &>>${LOG_FILE}
+STAT_CHECK $? "Service Enable And Start"
