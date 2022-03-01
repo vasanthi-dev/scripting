d13b958387f11ae208231fbf34cbcd2daca543d9
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-01 17:34:56 -0500

    component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,26 +5,42 @@ source components/common.sh
 #MAX_LENGTH=$( cat ${0} components/common.sh |grep STAT_CHECK |grep -v -W cat |awk -F '"' '{print$2}' |awk '{print length}' |sort |tail -1 )
 
 echo -e "\e[1mFrontend Installation\e[0m"
-echo -e "\e[1mNginx Installation\e[0m"
+
 yum install nginx -y &>>${LOG_FILE}
 STAT_CHECK $? "Nginx Installation"
 
-echo -e "\e[1mDownload the frontend Content\e[0m"
-#curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
-#STAT_CHECK $? "Download frontend Content"
 DOWNLOAD frontend
 
-echo -e "\e[1mRemove All Files\e[0m"
-rm -rf /usr/share/nginx/html/* &>>${LOG_FILE}
-STAT_CHECK $? "Remove All html content Files"
+rm -rf /usr/share/nginx/html/*
+STAT_CHECK $? "Remove old HTML Pages"
 
-echo -e "\e[1mMove files to nginx path\e[0m"
-mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>${LOG_FILE}
-STAT_CHECK $? "Move files to nginx path"
+cd  /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
+STAT_CHECK $? "Copying Frontend Content"
 
-echo -e "\e[1mCopy Roboshop Configuration File\e[0m"
-cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
-STAT_CHECK $? "Copy Roboshop Configuration File"
+cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $? "Copy Nginx Config File"
+
+
+#echo -e "\e[1mNginx Installation\e[0m"
+#yum install nginx -y &>>${LOG_FILE}
+#STAT_CHECK $? "Nginx Installation"
+#
+#echo -e "\e[1mDownload the frontend Content\e[0m"
+##curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
+##STAT_CHECK $? "Download frontend Content"
+#DOWNLOAD frontend
+#
+#echo -e "\e[1mRemove All Files\e[0m"
+#rm -rf /usr/share/nginx/html/* &>>${LOG_FILE}
+#STAT_CHECK $? "Remove All html content Files"
+#
+#echo -e "\e[1mMove files to nginx path\e[0m"
+#mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>${LOG_FILE}
+#STAT_CHECK $? "Move files to nginx path"
+#
+#echo -e "\e[1mCopy Roboshop Configuration File\e[0m"
+#cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
+#STAT_CHECK $? "Copy Roboshop Configuration File"
 
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/'\
        -e '/user/ s/localhost/user.roboshop.internal/'\
