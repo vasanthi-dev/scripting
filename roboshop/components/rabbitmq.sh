d6a726195ee4b9131bfd9cb46f725f4a1d824f1e
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-01 17:14:31 -0500

    component

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -3,17 +3,11 @@ source components/common.sh
 echo "\e[1mRabbitmq Installation\e[0m"
 #MAX_LENGTH=$( cat ${0} components/common.sh |grep STAT_CHECK |grep -v -W cat |awk -F '"' '{print$2}' |awk '{print length}' |sort |tail -1 )
 
-echo "\e[1mInstalling Erlang\e[0m"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>>${LOG_FILE}
-STAT_CHECK $? "Installing Erlang"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
+STAT_CHECK $? "Download RabbitMQ Repo"
 
-echo "\e[1mDownloading RabbitMQ Repo\e[0m"
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo  &>>${LOG_FILE}
-STAT_CHECK $? "Downloading RabbitMQ Repo"
-
-echo "\e[1mInstalling RabbitMQ\e[0m"
-yum install rabbitmq-server -y  &>>${LOG_FILE}
-STAT_CHECK $? "Installing RabbitMQ"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}
+STAT_CHECK $? "Install Erlang & RabbitMQ"
 
 echo"check Application user"
 rabbitmqctl list_user | grep roboshop &>>${LOG_FILE}
