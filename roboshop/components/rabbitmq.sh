b63a5a3f70ed3da06e88c9fb0114aba45ac86552
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-01 17:27:32 -0500

    component

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -9,12 +9,14 @@ STAT_CHECK $? "Download RabbitMQ Repo"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG_FILE}
 STAT_CHECK $? "Install Erlang & RabbitMQ"
 
-echo "check Application user"
-rabbitmqctl list_users | grep roboshop &>>${LOG_FILE}
+systemctl enable rabbitmq-server &>>${LOG_FILE}  && systemctl start rabbitmq-server &>>${LOG_FILE}
+
+rabbitmqctl  list_users | grep roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
-  rabbitmqctl add_user roboshop roboshop123
-  STAT_CHECK $? "Create Application user in RabbitMQ"
-  fi
-echo "Configure App User Permissions"
-rabbitmqctl set_user_tags roboshop administrator &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG_FILE}
-STAT_CHECK $? "Configure App User Permissions"
+  rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
+  STAT_CHECK $? "Create APp User in RabbitMQ"
+fi
+
+rabbitmqctl set_user_tags roboshop administrator  &>>${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>${LOG_FILE}
+STAT_CHECK $? "Configure APp User Permissions"
+
