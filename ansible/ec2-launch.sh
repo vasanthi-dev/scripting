74c41bc88a12d7aec1b1aad21f59eb7b94e68a41
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-04 11:27:04 -0500

    ansible simple playbook

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -2,4 +2,7 @@
 
 #aws ec2 request-spot-instances --instance-count 1 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" --type "persistent" --launch-specification file://spot.json |jq
 
-aws ec2 run-instances --launch-specification file://spot.json |jq
+#aws ec2 run-instances --launch-specification file://spot.json |jq
+
+
+aws ec2 run-instances --launch-template LaunchTemplateId="lt-099eb0b79a90eeba3",LaunchTemplateName="my-spot-req",Version="8"
