e5223dba43e9c8c300506aeb7fad1c5f394f4d91
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-04 11:46:42 -0500

    ansible simple playbook

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -10,4 +10,4 @@ TEMP_VER=8
 
 
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} |jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"  |jq
