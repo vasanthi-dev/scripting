16ca1effba97359fa2b22d839acaf44583e8129e
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-04 11:27:34 -0500

    ansible simple playbook

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -5,4 +5,4 @@
 #aws ec2 run-instances --launch-specification file://spot.json |jq
 
 
-aws ec2 run-instances --launch-template LaunchTemplateId="lt-099eb0b79a90eeba3",LaunchTemplateName="my-spot-req",Version="8"
+aws ec2 run-instances --launch-template LaunchTemplateId="lt-099eb0b79a90eeba3",LaunchTemplateName="my-spot-req",Version="8" |jq
