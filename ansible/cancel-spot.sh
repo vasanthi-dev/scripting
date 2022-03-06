c4c841a0ec532673236796cdf2442e2acf3aaefb
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-06 07:51:36 -0500

    creating multiple instances at one time

--- ansible/cancel-spot.sh
+++ ansible/cancel-spot.sh
@@ -4,12 +4,12 @@
 #aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${comp}" --query "SpotInstanceRequests[*].[InstanceId]"
 
 
-  for comp in test test2 ; do
-  SPOT_INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${comp}" --query "SpotInstanceRequests[*].[SpotInstanceRequestId]" |jq)
+  for comp in test1 test2 ; do
+  SPOT_INSTANCE_ID[]=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${comp}" --query "SpotInstanceRequests[*].[SpotInstanceRequestId]" |jq)
 
-  echo "${SPOT_INSTANCE_ID}"
+  echo "${SPOT_INSTANCE_ID[]}"
 
-  aws ec2 cancel-spot-instance-requests --spot-instance-request-ids "${SPOT_INSTANCE_ID}"
+  aws ec2 cancel-spot-instance-requests --spot-instance-request-ids "${SPOT_INSTANCE_ID[]}"
 
   done
 
