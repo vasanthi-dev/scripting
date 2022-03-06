9aaffca9f5a6dd0bde2f1a924fada9100410fb56
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-06 05:29:27 -0500

    create instance

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -18,21 +18,33 @@
 TEMP_ID="lt-099eb0b79a90eeba3"
 TEMP_NAME="my-spot-req"
 TEMP_VER=8
+ZONE_ID="Z01304753HAJF01BJ7NCL"
+
 if [ -z "$1" ]; then
-  echo "Input is missing"
+  echo -e  "\e[1;31mInput is missing\e[0m"
   exit 1
   fi
+
 COMPONENT=$1
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name |sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 
 if [ $? -eq -0 ]; then
-  echo "Instance already exist"
-  else
-    aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  |jq
+  echo -e "\e[1;33mInstance already exist\e[0m"
+  exit
   fi
 
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  |jq
 
+
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=workstation" | jq ".Reservations[].Instances[].PrivateIpAddress" |xargs)
+
+#otherway to eleminate Double quotes
+# IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=workstation" | jq ".Reservations[].Instances[].PrivateIpAddress" | sed 's/"//g')
+
+sed -e 's/IPADDRESS/${IPADDRESS}/' -e 's/COMPONENT/${COMPONENT}/' dnsrecord.json >/tmp/record.json
+
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 
 
 
