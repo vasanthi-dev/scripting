94c449c13408c26ad47ded873acb22e0bfbb19dc
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-06 04:50:09 -0500

    create instance

--- ansible/ec2-launch.sh
+++ ansible/ec2-launch.sh
@@ -20,12 +20,13 @@ TEMP_NAME="my-spot-req"
 TEMP_VER=8
 if [ -z "$1" ]; then
   echo "Input is missing"
+  exit 1
   fi
 COMPONENT=$1
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name |sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 
-if [ $? -eq 0 ]; then
+if [ $? -eq -0 ]; then
   echo "Instance already exist"
   else
     aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  |jq
