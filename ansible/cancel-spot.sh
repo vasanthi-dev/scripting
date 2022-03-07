15b9cd3ca8802417f725e7a8873762a108912b32
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-07 04:28:23 -0500

    creating multiple instances at one time

--- ansible/cancel-spot.sh
+++ ansible/cancel-spot.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-
-#aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${comp}" --query "SpotInstanceRequests[*].[InstanceId]"
-
 STATUS="active"
   for comp in test1 test2 ; do
   SPOT_INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${comp} Name=state,Values=${STATUS}|jq ".SpotInstanceRequests[].SpotInstanceRequestId" |xargs)
-
+  INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${comp} Name=state,Values=${STATUS}|jq ".SpotInstanceRequests[].InstanceId" |xargs)
   echo "${SPOT_INSTANCE_ID}"
-
+   echo "${INSTANCE_ID}"
   aws ec2 cancel-spot-instance-requests --spot-instance-request-ids "${SPOT_INSTANCE_ID}"
-
+  aws ec2 terminate-instances --instance-ids "${INSTANCE_ID}"
   done
 
 
-
+#aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${comp}" --query "SpotInstanceRequests[*].[InstanceId]"
 #aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${test1}" |jq ".SpotInstanceRequests[*].[SpotInstanceRequestId]" |jq
 #
 #
@@ -25,4 +22,4 @@ STATUS="active"
 #
 #aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=test1 Name=state,Values=active |jq ".SpotInstanceRequests[].SpotInstanceRequestId"
 #
-#aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=test1 Name=state,Values=active |jq ".SpotInstanceRequests[].SpotInstanceRequestId"|xargs
+#aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=test1 Name=state,Values=active |jq ".SpotInstanceRequests[].InstanceId"|xargs
