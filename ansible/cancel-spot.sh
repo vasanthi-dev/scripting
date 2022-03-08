b90e11b7c4d7bbe8ca60d051bccc067ff5131cb0
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-08 15:32:02 -0500

    cancel spot request change

--- ansible/cancel-spot.sh
+++ ansible/cancel-spot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ENV=$1
+
+if [ ! -z "$ENV" ]; then
+  ENV="-${ENV}"
+  fi
+
 STATUS="active"
   for comp in frontend$ENV mongodb$ENV user$ENV catalogue$ENV mysql$ENV redis$ENV cart$ENV shipping$ENV payment$ENV rabbitmq$ENV dispatch$ENV ; do
   SPOT_INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${comp} Name=state,Values=${STATUS}|jq ".SpotInstanceRequests[].SpotInstanceRequestId" |xargs)
